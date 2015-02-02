e21b6a9198d68fe0fc94c2b36e29d435de4d1f3e
Jean-Michel Royer <jeanmichelroyer@gmail.com>
2015-02-02 22:50:39 +0100

    Update _prepend.php

--- _prepend.php
+++ _prepend.php
@@ -2,4 +2,22 @@
 
 if (!defined('DC_RC_PATH')) {return;}
 
+# ACTUALITES
+$core->url->register('actualite', 'actualite', 'actualite', function() { self::serveDocument('actualite.html','text/html'); });
+
+
+# BIOGRAPHIE
+$core->url->register('artiste', 'artiste', 'artiste', function() { self::serveDocument('artiste.html','text/html'); });
+
+# DISCOGRAPHIE
+# — SIMPLE
+$core->url->register('support', 'support', 'discographie/support', function() { self::serveDocument('support.html','text/html'); });
+
+
+# MEDIATHEQUE
+# — LIST
+
+# — SIMPLE
+$core->url->register('photo', 'photo', 'mediatheque/photo', function() { self::serveDocument('photo.html','text/html'); });
+$core->url->register('video', 'video', 'mediatheque/video', function() { self::serveDocument('video.html','text/html'); });
 $core->url->register('presse', 'presse', 'mediatheque/presse', function() { self::serveDocument('presse.html','text/html'); });
